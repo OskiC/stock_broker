e61d0cc48541f5b531b555bfbf42b0cec363ac70
OskiC <xoskarx1@gmail.com>
2025-03-05 15:38:47 +0100

    init

---
+++ src/test/java/com/stock/stock_broker/StockBrokerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.stock.stock_broker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StockBrokerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
