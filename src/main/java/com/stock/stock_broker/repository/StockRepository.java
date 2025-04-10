58b0cf83f5d1c2c92d90455285e6d2a7d8c12eea
OskiC <xoskarx1@gmail.com>
2025-04-10 18:03:31 +0200

    refactor StockRepository

--- src/main/java/com/stock/stock_broker/repository/StockRepository.java
+++ src/main/java/com/stock/stock_broker/repository/StockRepository.java
@@ -2,7 +2,6 @@ package com.stock.stock_broker.repository;
 
 import com.stock.stock_broker.model.Stock;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.orm.jpa.support.OpenEntityManagerInViewFilter;
 import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
@@ -10,7 +9,7 @@ import java.util.Optional;
 @Repository
 public interface StockRepository extends JpaRepository<Stock, Long> {
     Optional<Stock> findById(Long id);
-    Optional<Stock> findByName(String stockName);
+    Optional<Stock> findByStockName(String stockName);
     Optional<Stock> findByCodeName(String codeName);
-    boolean stockExists(String stockName, String codeName);
+    boolean existsByStockNameAndCodeName(String stockName, String codeName);
 }
