4488201600749e2a18f66a196d2983b2a659418c
OskiC <xoskarx1@gmail.com>
2025-04-13 16:53:26 +0200

    add getUserPortfolio endpoint with logic in the server

--- src/main/java/com/stock/stock_broker/controller/UserController.java
+++ src/main/java/com/stock/stock_broker/controller/UserController.java
@@ -1,11 +1,15 @@
 package com.stock.stock_broker.controller;
 
-import com.stock.stock_broker.dto.UserRegistrationDTO;
+import com.stock.stock_broker.dto.user.PortfolioDTO;
+import com.stock.stock_broker.dto.user.UserRegistrationDTO;
 import com.stock.stock_broker.model.User;
 import com.stock.stock_broker.service.UserService;
+import org.apache.coyote.Response;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/users")
@@ -24,5 +28,11 @@ public class UserController {
     public ResponseEntity<User> getUserById(@PathVariable Long userId){
         User userFound = userService.getUserById(userId);
         return ResponseEntity.ok(userFound);
+    }
+
+    @GetMapping("/{userId}/portfolio")
+    public ResponseEntity<List<PortfolioDTO>> getPortfolio(@PathVariable Long userId){
+        List<PortfolioDTO> portfolio = userService.getUserPortfolio(userId);
+        return ResponseEntity.ok(portfolio);
     }
 }
--- src/main/java/com/stock/stock_broker/service/UserService.java
+++ src/main/java/com/stock/stock_broker/service/UserService.java
@@ -1,10 +1,19 @@
 package com.stock.stock_broker.service;
 
+import com.stock.stock_broker.dto.user.PortfolioDTO;
+import com.stock.stock_broker.model.Stock;
+import com.stock.stock_broker.model.Transaction;
 import com.stock.stock_broker.model.User;
+import com.stock.stock_broker.repository.TransactionRepository;
 import com.stock.stock_broker.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
 
 @Service
 public class UserService {
@@ -12,6 +21,8 @@ public class UserService {
     private UserRepository userRepository;
 
     private final BCryptPasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
+    @Autowired
+    private TransactionRepository transactionRepository;
 
     public User registerUser(String username, String password){
         if(userRepository.existsByUsername(username)){
@@ -29,5 +40,38 @@ public class UserService {
     public User getUserById(Long id){
         return userRepository.findById(id)
                 .orElseThrow(() -> new IllegalArgumentException("User not found"));
+    }
+
+    public List<PortfolioDTO> getUserPortfolio(Long id){
+        User user = getUserById(id);
+
+        List<Transaction> transactions = transactionRepository.findByUserId(id);
+
+        Map<Stock, List<Transaction>> grouped = transactions.stream()
+                .filter(t -> t.getClosePrice() == null).collect(Collectors.groupingBy(Transaction::getStock));
+
+        List<PortfolioDTO> result = new ArrayList<>();
+
+        for(Map.Entry<Stock, List<Transaction>> entry : grouped.entrySet()){
+            Stock stock = entry.getKey();
+            List<Transaction> userTransactions = entry.getValue();
+
+            int quantity = userTransactions.stream().mapToInt(Transaction::getQuantity).sum();
+            double avgBuyPrice = userTransactions.stream().
+                    mapToDouble(t -> t.getOpenPrice() * t.getQuantity()).sum() / quantity;
+            double currentPrice = stock.getPrice();
+            double totalValue = stock.getPrice() * quantity;
+            double PnL = quantity * (currentPrice - avgBuyPrice);
+
+            result.add(new PortfolioDTO(
+                    stock.getStockName(),
+                    quantity,
+                    avgBuyPrice,
+                    currentPrice,
+                    totalValue,
+                    PnL
+            ));
+        }
+        return result;
     }
 }
